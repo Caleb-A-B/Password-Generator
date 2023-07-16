588b0d6f4079980e5b2180a57fbfff4a242622f9
caleb baca <calebabaca@gmail.com>
2023-07-16 15:54:31 -0700

    organization of java

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
 var enter;
-var number= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var uppercase;
-var lowercase = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
-var character; 
+var numberArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var uppercaseArr = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+var lowercaseArr = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+var characterArr = ["!", "@", "#", "$", "%", "^", "&", "*",]
+var password = "";
+//var "title" confirm area
+
+//arrays holding ocnfirm
+var combine= [];
+//validate input
+
 
 // Write password to the #password input
 function writePassword() {
@@ -23,7 +30,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//make an else if section for each type, use true/false
 
+//wriiting password
+function writePassword()
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
